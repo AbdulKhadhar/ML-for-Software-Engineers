a1583d467e5695df9986b10e6b0cf7c422a49589
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-04 22:50:42 +0530

    Create nonLeniarFun.py

---
+++ Numpy/Math/nonLeniarFun.py
@@ -0,0 +1,11 @@
+arr = np.array([[1, 2], [3, 4]])
+# Raised to power of e
+print(repr(np.exp(arr)))
+# Raised to power of 2
+print(repr(np.exp2(arr)))
+
+arr2 = np.array([[1, 10], [np.e, np.pi]])
+# Natural logarithm
+print(repr(np.log(arr2)))
+# Base 10 logarithm
+print(repr(np.log10(arr2)))

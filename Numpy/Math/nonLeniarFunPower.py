d3132f4089b3cd78eb9c97855c130a8fe61d5458
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-04 22:51:32 +0530

    Create nonLeniarFunPower.py

---
+++ Numpy/Math/nonLeniarFunPower.py
@@ -0,0 +1,6 @@
+arr = np.array([[1, 2], [3, 4]])
+# Raise 3 to power of each number in arr
+print(repr(np.power(3, arr)))
+arr2 = np.array([[10.2, 4], [3, 5]])
+# Raise arr2 to power of each number in arr
+print(repr(np.power(arr2, arr)))

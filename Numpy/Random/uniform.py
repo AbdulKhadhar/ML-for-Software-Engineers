9f3d3d799ffea307ce025fa3a2335035410806ed
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-05 21:16:49 +0530

    Create uniform.py

---
+++ Numpy/Random/uniform.py
@@ -0,0 +1,5 @@
+print(np.random.uniform())
+print(np.random.uniform(low=-1.5, high=2.2))
+print(repr(np.random.uniform(size=3)))
+print(repr(np.random.uniform(low=-3.4, high=5.9,
+                             size=(2, 2))))

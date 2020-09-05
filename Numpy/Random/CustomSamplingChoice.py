596ba0f694b2635872caee6ca547d9f26297adfb
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-05 21:26:35 +0530

    Create CustomSamplingChoice.py

---
+++ Numpy/Random/CustomSamplingChoice.py
@@ -0,0 +1,5 @@
+colors = ['red', 'blue', 'green']
+print(np.random.choice(colors))
+print(repr(np.random.choice(colors, size=2)))
+print(repr(np.random.choice(colors, size=(2, 2),
+                            p=[0.8, 0.19, 0.01])))

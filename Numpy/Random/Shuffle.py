0f431bce39f0ef965e29292c756137dc0f5d7e3b
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-05 21:13:46 +0530

    Create Shuffle.py

---
+++ Numpy/Random/Shuffle.py
@@ -0,0 +1,11 @@
+vec = np.array([1, 2, 3, 4, 5])
+np.random.shuffle(vec)
+print(repr(vec))
+np.random.shuffle(vec)
+print(repr(vec))
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+np.random.shuffle(matrix)
+print(repr(matrix))

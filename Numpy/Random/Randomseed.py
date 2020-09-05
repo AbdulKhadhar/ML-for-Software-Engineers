db523e42b29e7eff676e0ce742ba65f5c3a24b1c
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-05 21:12:19 +0530

    Create Randomseed.py

---
+++ Numpy/Random/Randomseed.py
@@ -0,0 +1,19 @@
+np.random.seed(1)
+print(np.random.randint(10))
+random_arr = np.random.randint(3, high=100,
+                               size=(2, 2))
+print(repr(random_arr))
+
+# New seed
+np.random.seed(2)
+print(np.random.randint(10))
+random_arr = np.random.randint(3, high=100,
+                               size=(2, 2))
+print(repr(random_arr))
+
+# Original seed
+np.random.seed(1)
+print(np.random.randint(10))
+random_arr = np.random.randint(3, high=100,
+                               size=(2, 2))
+print(repr(random_arr))

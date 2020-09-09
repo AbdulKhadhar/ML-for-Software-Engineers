9c4ccefe070c06514e2d10a1431e7dab7d6837ef
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-09 22:27:42 +0530

    Create FilteringData.py

---
+++ Numpy/Filtering/FilteringData.py
@@ -0,0 +1,8 @@
+arr = np.array([[0, 2, 3],
+                [1, 3, -6],
+                [-3, -2, 1]])
+print(repr(arr == 3))
+print(repr(arr > 0))
+print(repr(arr != 1))
+# Negated from the previous step
+print(repr(~(arr != 1)))
